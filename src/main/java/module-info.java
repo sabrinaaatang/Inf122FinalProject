d850072b8a90e1a70af2645f0624d1cc955794f4
Madeline Annison <maddie@theannisons.com>
2025-03-15 22:51:21 -0700

    fix missing import

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.openjfx.inf122finalproject {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens org.openjfx.inf122finalproject to javafx.fxml;
     exports org.openjfx.inf122finalproject;
--- src/main/java/org/openjfx/inf122finalproject/GameManager.java
+++ src/main/java/org/openjfx/inf122finalproject/GameManager.java
@@ -10,7 +10,7 @@ public abstract class GameManager {
 
     }
 
-    void handleInput(PlayerInput input) {
-        
-    }
+//    void handleInput(PlayerInput input) {
+//
+//    }
 }
